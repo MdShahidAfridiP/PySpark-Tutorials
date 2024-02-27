46327bb12f021cc3c49280fe90544b05d9addd88
MdShahidAfridiP <mdshahidafridip@gmail.com>
2024-02-27 15:59:53 +0530

    added flatmap

---
+++ pyspark-dataframe-flatMap.py
@@ -0,0 +1,23 @@
+from pyspark.sql import SparkSession, Row
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+columns = ["name","languagesAtSchool","currentState"]
+data = [("James,,Smith",["Java","Scala","C++"],"CA"), \
+    ("Michael,Rose,",["Spark","Java","C++"],"NJ"), \
+    ("Robert,,Williams",["CSharp","VB"],"NV")]
+
+# Convert data to a DataFrame
+rdd = spark.sparkContext.parallelize(data)
+row_rdd = rdd.map(lambda x: Row(name=x[0], languagesAtSchool=x[1], currentState=x[2]))
+df = spark.createDataFrame(row_rdd, columns)
+
+# Apply flatMap transformation
+flat_mapped_df = df.rdd.flatMap(lambda x: [(x["name"], lang, x["currentState"]) for lang in x["languagesAtSchool"]])
+
+# Convert result to DataFrame
+result_columns = ["name", "language", "currentState"]
+result_df = flat_mapped_df.toDF(result_columns)
+
+# Show the result
+result_df.show() 
+
